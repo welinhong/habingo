df543d0e905c5c0faa35637e779c6b32028f0df5
Welin Hong <danbi@healingpaper.com>
2021-04-20 08:08:28 +0900

    update eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,18 +20,25 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
-  
+
   plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off', // No need to import React with Next.js
     '@typescript-eslint/no-empty-interface': 0,
     'prettier/prettier': 0,
     'react/prop-types': ['error', { ignore: ['navigation', 'route'] }],
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-return': 0,
+    'react/prop-types': 0,
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
-  ignorePatterns: ['node_modules/*'], 
+  ignorePatterns: ['node_modules/*'],
 }
+
+
