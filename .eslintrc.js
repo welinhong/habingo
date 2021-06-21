1d49f57744c8b738aeb2098eba06215c8a26e773
Welin Hong <danbi@healingpaper.com>
2021-06-22 07:38:51 +0900

    update eslint config: no-floating-promises, no-empty-function

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,15 +24,17 @@ module.exports = {
   plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off', // No need to import React with Next.js
-    '@typescript-eslint/no-empty-interface': 0,
     'prettier/prettier': 0,
+    'react/display-name': 0,
+    'react/prop-types': 0,
     'react/prop-types': ['error', { ignore: ['navigation', 'route'] }],
+    '@typescript-eslint/no-empty-interface': 0,
     '@typescript-eslint/no-unsafe-assignment': 0,
     '@typescript-eslint/no-unsafe-member-access': 0,
     '@typescript-eslint/no-unsafe-call': 0,
     '@typescript-eslint/no-unsafe-return': 0,
-    'react/prop-types': 0,
-    'react/display-name': 0,
+    '@typescript-eslint/no-floating-promises': 0,
+    '@typescript-eslint/no-empty-function': 0,
   },
   settings: {
     react: {
